1717136b8ee1ad01a323a99bb9e7acc7b60934e0
Keshav <keshav2021csds116@abesit.edu.in>
2024-10-16 02:34:31 +0530

    Initialized

---
+++ src/screens/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='bg-black text-white flex justify-center items-center w-screen h-screen'>
+    GharSetu V2 Coming Soon...
+   </div>
+  )
+}
+
+export default Home
