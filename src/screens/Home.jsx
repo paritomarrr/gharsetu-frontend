8f5f7b7bef176ba6df7025bcc6652da6dbb471bf
Keshav <keshav2021csds116@abesit.edu.in>
2024-10-16 02:36:32 +0530

    Adding Dev Branch

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className='bg-black text-white flex justify-center items-center w-screen h-screen'>
-    GharSetu V2 Coming Soon...
+    GharSetu V2 Coming Soon.....
    </div>
   )
 }
