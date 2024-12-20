3d57a4f0fcc230ba0e8b9faf6d090fcf98cf4698
prakhar5 <prakhar@ith.tech>
2024-12-20 11:41:56 +0530

    Fix: file name

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from '../src/store/store.js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import TagManager from 'react-gtm-module'
-import { UserProvider } from './context/userContext'
+import { UserProvider } from './context/UserContext'
 
 const tagManagerArgs = {
   gtmId: 'G-XEM56GK8PH'
